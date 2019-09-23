72b34dc59d169e1fbdb5b4bdf3b2837da785889b
betocostadev <robertomarx@gmail.com>
2019-09-23 12:14:52 -0300

    added memoization example

---
+++ memoization.js
@@ -0,0 +1,24 @@
+const addTo80 = n => {
+  console.log('long time...');
+  return n + 80;
+}
+
+console.log(addTo80(10));
+console.log(addTo80(10));
+
+const cache = {};
+console.log(cache);
+const memoizedAddTo80 = n => {
+  if (n in cache) {
+    console.log('found in cache!');
+    return cache[n];
+  } else {
+    console.log('long time');
+    cache[n] = n + 80;
+    return cache[n];
+  }
+}
+
+console.log('1', memoizedAddTo80(10));
+console.log('2', memoizedAddTo80(10));
+console.log(cache);
