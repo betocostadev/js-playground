0f61399a57e3fefa25607e764ed874e856329701
betocostadev <robertomarx@gmail.com>
2024-07-23 19:21:59 -0300

    Solved the two crystal balls problem

--- challenges/theTwoCristalBall.js
+++ challenges/theTwoCristalBall.js
@@ -1,4 +1,50 @@
 // The two crystal ball problem
-// Given two crystal balls that will break if dropped from high enough
-// distance, determine the exact spot in which it will break in the most
-// optimized way.
+// Given two crystal balls that will break if dropped from high enough distance,
+// determine the exact spot in which it will break in the most optimized way.
+
+// Options - Linear and Binary Search
+// Think like this [f,f,f,f,f,t,t,t,t] - Remember that there are Two Crystal Balls
+
+// A good solution is to walk a squareRoot of N, and when the first ball breaks
+// go back from the last position (the last square root), then forward again
+
+// O(Vn + vN)
+
+function findOptimalBreakDistance(breaks) {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length))
+
+  let i = jumpAmount
+
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) {
+      break
+    }
+  }
+
+  // Jump back (imagine the first ball broke, we will go back to check where it can break before this point)
+  i -= jumpAmount
+
+  for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+    // Walk maximum a jump amount of steps
+    if (breaks[i]) {
+      return i
+    }
+  }
+
+  return -1
+}
+
+// Test cases
+
+let idx = Math.floor(Math.random() * 10000)
+const data = new Array(10000).fill(false)
+
+for (let i = idx; i < 10000; ++i) {
+  data[i] = true
+}
+
+console.log(findOptimalBreakDistance(data))
+console.log(
+  findOptimalBreakDistance([false, false, false, false, false, true, true, true, true, true])
+)
+console.log(findOptimalBreakDistance([false, false, false, false, false, false]))
--- dsa-learning/fendmasters/kata-machine/src/day1/TwoCrystalBalls.ts
+++ dsa-learning/fendmasters/kata-machine/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            // Reached our first true statement
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+
+    for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            // Found the second true - return the index of it
+            return i;
+        }
+    }
+
+    return -1;
 }
