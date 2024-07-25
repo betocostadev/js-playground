ea8cd566f0e6f332c3325c9bdbebbb97109e9617
betocostadev <robertomarx@gmail.com>
2024-07-25 11:15:31 -0300

    Improved Bubble sort using swaps

--- dsa-learning/sort/bubbleSort.js
+++ dsa-learning/sort/bubbleSort.js
@@ -4,7 +4,9 @@
 // O (N - N + 1)
 // O N(N + 1)/2 = (N2 + N)
 // Its O (N2)
-const bubbleSort = (arr) => {
+
+// No swaps
+const bubbleSortNoSwaps = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -17,11 +19,43 @@ const bubbleSort = (arr) => {
   return arr
 }
 
-const arr1 = [0, 5, 1, 3, 6, 2, 10]
-const arr2 = [1, 5, 0, 3, 6, 2, 11, 4, 9, 22]
+const bubbleSort = (arr) => {
+  let swaps = -1
+  for (let i = 0; i < arr.length - 1; i++) {
+    swaps = 0
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tmp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
+        swaps++
+      }
+    }
+    if (swaps === 0) return arr
+  }
+}
 
-const sortedArr1 = bubbleSort(arr1)
-const sortedArr2 = bubbleSort(arr2)
+const arr1 = [
+  0, 5, 1, 2, 3, 6, 39, 10, 13, 12, 11, 24, 29, 23, 22, 21, 30, 4, 14, 16, 15, 19, 18, 17, 38, 37,
+  35, 33, 32, 28, 25, 31, 26, 27, 20,
+]
+const arr2 = [21, 2, 3, 5, 4, 6, 7, 13, 8, 9, 10, 11, 12, 16, 22]
+const arr3 = [
+  0, 5, 1, 2, 3, 6, 39, 10, 13, 12, 11, 24, 29, 23, 22, 21, 30, 4, 14, 16, 15, 19, 18, 17, 38, 37,
+  35, 33, 32, 28, 25, 31, 26, 27, 20,
+]
+const arr4 = [21, 2, 3, 5, 4, 6, 7, 13, 8, 9, 10, 11, 12, 16, 22]
 
+console.time('No swaps time')
+const sortedArr1 = bubbleSortNoSwaps(arr1)
+const sortedArr2 = bubbleSortNoSwaps(arr2)
+console.timeEnd('No swaps time') // ~0.165ms
 console.log(sortedArr1)
 console.log(sortedArr2)
+
+console.time('Swaps time')
+const sortedArr3 = bubbleSort(arr3)
+const sortedArr4 = bubbleSort(arr4)
+console.timeEnd('Swaps time') // ~0.060ms
+console.log(sortedArr3)
+console.log(sortedArr4)
