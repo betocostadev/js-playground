f0b36c3c4f9cf2701d7b9756d45bbb0cd79afe71
betocostadev <robertomarx@gmail.com>
2024-07-29 12:07:42 -0300

    Finished Maze Solver

--- dsa-learning/algorithms/rec-maze-solver.ts
+++ dsa-learning/algorithms/rec-maze-solver.ts
@@ -1,9 +1,36 @@
 // Recursion
 // Maze Solver
 
+// Maze
+
+// ["#####E#",
+//  "#     #"
+//  "#S#####"]
+
+// Create a directions array to help
+
 // Base Case:
 // 1 - Off the map
 // 2 - It's a Wall
 // 3 - Reached goal
 // 4 - If it was there before
+
+function walk(
+  maze: string[],
+  wall: string,
+  curr: Point,
+  end: Point,
+  seen: boolean[][],
+  path: Point[]
+): boolean {
+  // - Base case
+  // Off the map
+  // On a Wall
+  // Found the ending
+  // Seen it before
+  // - Recursion
+  // pre
+  // recurse
+  // post
+}
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {}
--- dsa-learning/fendmasters/kata-machine/src/day1/MazeSolver.ts
+++ dsa-learning/fendmasters/kata-machine/src/day1/MazeSolver.ts
@@ -1,8 +1,97 @@
-function walk(maze: string[], wall: string, curr: Point, end: Point): boolean {}
+const dir = [
+    [-1, 0], // left
+    [1, 0], // down
+    [0, -1], // right
+    [0, 1], // up
+];
+
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+    path: Point[],
+): boolean {
+    // Base case
+
+    // Off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+
+    // On a Wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // Found the ending
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
+        return true;
+    }
+
+    // Seen it before
+    if (seen[curr.y][curr.x] === true) {
+        return false;
+    }
+
+    // 3 Recurse
+    // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // recurse
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        if (
+            walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path)
+        ) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+
+    return false;
+}
 
 export default function solve(
     maze: string[],
     wall: string,
     start: Point,
     end: Point,
-): Point[] {}
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; i++) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
+}
+
+[
+    -"xxxxxxxxxx*x",
+    -"x        x*x",
+    -"x        x*x",
+    -"x xxxxxxxx*x",
+    -"x**********x",
+    -"x*xxxxxxxxxx",
+
+    +"xxxxxxxxxx x",
+    +"x        x x",
+    +"x        x x",
+    +"x xxxxxxxx x",
+    +"x          x",
+    +"x xxxxxxxxxx",
+];
