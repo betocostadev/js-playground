d8a6bfb059ddd18443da38777ca4529fbf4a15ed
betocostadev <robertomarx@gmail.com>
2024-07-30 16:36:52 -0300

    Added Quick Sort

--- dsa-learning/fendmasters/kata-machine/src/day1/QuickSort.ts
+++ dsa-learning/fendmasters/kata-machine/src/day1/QuickSort.ts
@@ -1,31 +1,50 @@
-export default function quick_sort(arr: number[]): void {
-    if (arr.length === 0 || arr.length === 1) {
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
         return;
     }
 
-    const p = arr[Math.floor(arr.length / 2)];
-    const low = [];
-    const high = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] <= p) {
-            low.push(arr[i]);
-        } else {
-            high.push(arr[i]);
+    const pivotIdx = partition(arr, low, high);
+
+    qs(arr, low, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, high);
+
+    console.log("Solved Arr: ", arr);
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    // const pivot = arr[Math.floor(arr.length / 2)];
+    const pivot = arr[high];
+
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        // for (let i = low; i < arr.length; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
         }
     }
-    console.log(p);
-    console.log(low);
-    console.log(high);
 
-    const left = quick_sort(low);
-    const right = quick_sort(high);
-    console.log(arr);
-    console.log(left);
-    console.log(right);
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
 
 const thing = [
     26, 21, 1, 3, 6, 5, 20, 22, 14, 15, 19, 10, 8, 9, 7, 18, 21, 12, 13,
 ];
 
-quick_sort(thing);
+const testArr = [9, 3, 7, 4, 69, 420, 42];
+
+// console.log(quick_sort(testArr));
+console.log(quick_sort(thing));
+
+// quick_sort(thing);
