6dec025da6f6e986be363eaa7df89278d603774d
betocostadev <robertomarx@gmail.com>
2024-07-31 14:23:38 -0300

    Started adding optimized version of Quick Sort

--- dsa-learning/fendmasters/kata-machine/src/day1/QuickSort.ts
+++ dsa-learning/fendmasters/kata-machine/src/day1/QuickSort.ts
@@ -10,11 +10,13 @@ function qs(arr: number[], low: number, high: number): void {
 }
 
 function partition(arr: number[], low: number, high: number): number {
-    // const pivot = arr[Math.floor(arr.length / 2)];
-    const pivot = arr[high];
+    // Broken on purpose to change the pivot to the midpoint of the array
+    const pivot = arr[Math.floor(arr.length / 2)];
+    // const pivot = arr[high];
 
     let idx = low - 1;
-
+    // [9, 3, 7, 4, 69, 420, 42] - target [3, 4, 7, 69, 420, 42]
+    // [3, 7, 4, 9, 69, 420, 42]
     for (let i = low; i < high; i++) {
         // for (let i = low; i < arr.length; i++) {
         if (arr[i] <= pivot) {
@@ -22,7 +24,17 @@ function partition(arr: number[], low: number, high: number): number {
             const tmp = arr[i];
             arr[i] = arr[idx];
             arr[idx] = tmp;
+        } else {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = tmp;
         }
+        // idx = -1
+        // high = 6
+        // low = 0
+        // arr[0] = 9
+        // pivot = 4
     }
 
     idx++;
@@ -33,18 +45,18 @@ function partition(arr: number[], low: number, high: number): number {
 }
 
 export default function quick_sort(arr: number[]): void {
-    // const result = qs(arr, 0, arr.length - 1);
-    // console.log("Solved Arr: ", arr);
-    qs(arr, 0, arr.length - 1);
+    const result = qs(arr, 0, arr.length - 1);
+    console.log("Solved Arr: ", arr);
+    // qs(arr, 0, arr.length - 1);
 }
 
-const thing = [
-    26, 21, 1, 3, 6, 5, 20, 22, 14, 15, 19, 10, 8, 9, 7, 18, 21, 12, 13,
-];
+// const thing = [
+//     26, 21, 1, 3, 6, 5, 20, 22, 14, 15, 19, 10, 8, 9, 7, 18, 21, 12, 13,
+// ];
 
 const testArr = [9, 3, 7, 4, 69, 420, 42];
 
 console.log(quick_sort(testArr));
-console.log(quick_sort(thing));
+// console.log(quick_sort(thing));
 
 // quick_sort(thing);
