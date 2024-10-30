7f7f4e97c879014564d9f161863a925e742363c1
betocostadev <robertomarx@gmail.com>
2024-10-30 19:42:03 -0300

    Improved Two Crystal Balls problem

--- challenges/TwoCrystalBalls.js
+++ challenges/TwoCrystalBalls.js
@@ -1,4 +1,4 @@
-// The two crystal ball problem
+// The Two Crystal balls problem
 // Given two crystal balls that will break if dropped from high enough distance,
 // determine the exact spot in which it will break in the most optimized way.
 // Think about dropping the balls from a hundred-story building. Which floor is high enough
@@ -11,7 +11,12 @@
 // A good solution is to walk a squareRoot of N, and when the first ball breaks
 // go back from the last position (the last square root), then forward again
 // O(Vn + vN)
+
+// V25 = 5..
+// If the breaking point was 16, we would jump to 5, 10, 15, 20. At 20 the first ball breaks.
+// We go back to 15 and will go linearly but only from 15 to 20 in this case
 function findOptimalBreakDistance(breaks) {
+  // We jump the square root of any forward every time, until it breaks, then we can go back
   const jumpAmount = Math.floor(Math.sqrt(breaks.length))
   let i = jumpAmount
   for (; i < breaks.length; i += jumpAmount) {
