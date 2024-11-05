6aed52e174a88d5a636bdd21a22d34f83569d972
betocostadev <robertomarx@gmail.com>
2024-11-05 10:48:21 -0300

    Feat: Improved SumInRange formula explanation

--- challenges/sumInRange.js
+++ challenges/sumInRange.js
@@ -1,7 +1,15 @@
+// Sum All numbers in Range
 // Sum all the numbers from 0 until you reach the target.
 // Example: Target(10), 0 + 1, 1 + 2, 3 + 3, ...
 // The idea is to not use a sum like an array, also, not sum using a loop.
-// This is possible by using a simple formula:
+// This is possible by using a simple formula.
+// Pay attention to the sum below:
+// 1...100 = 101
+// 2...99 = 101
+// 3...98 = 101
+// ...
+// 50...51 = 101
+// 101 x 50 = Sum
 // (N + 1) * (N / 2)
 var sumToTarget = function (target) {
     var targetMax = target + 1;
