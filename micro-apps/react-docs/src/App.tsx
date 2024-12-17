cfe926cee08eb3c472660412aebcb0180583f48e
betocostadev <robertomarx@gmail.com>
2024-12-17 16:40:35 -0300

    Added React Docs

---
+++ micro-apps/react-docs/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h2>Vite + React</h2>
+      <p>This is a simple Web Application created using Vite and React.</p>
+      <p>It's main focus is on React documentation to explain and leverage React features.</p>
+    </>
+  )
+}
+
+export default App
