724fced0a51b4d172b0813df28d0192a99e66dad
betocostadev <robertomarx@gmail.com>
2025-01-29 17:49:51 -0300

    Small improvement to Linear search

--- dsa-learning/algorithms/linear-search.js
+++ dsa-learning/algorithms/linear-search.js
@@ -1,5 +1,7 @@
 // The most basic way of searching
 // Not the most performant one O(n)
+// This linear search will work as indexOf - It will return the position of the item
+// if the item is in the array
 var linearSearch = function (needle, haystack) {
     for (var i = 0; i < haystack.length; i++) {
         if (needle === haystack[i]) {
@@ -8,5 +10,13 @@ var linearSearch = function (needle, haystack) {
     }
     return -1;
 };
-var arr = [0, 2, 4, 12, 98, 75, 83, 3, 92, 99, 77, 33, 11, 22, 44, 534, 93, 13, 10];
-console.log(linearSearch(4, arr));
+console.log('===== Linear Search =====');
+var arr = [
+    0, 2, 4, 12, 98, 75, 83, 3, 92, 99, 77, 33, 11, 22, 44, 534, 93, 13, 10, 85, 84, 38, 88, 32, 10,
+];
+console.log('Array is: ');
+console.log(arr);
+console.log('What is the position of 4 in the array?', linearSearch(4, arr));
+console.log('What is the position of 99 in the array?', linearSearch(99, arr));
+console.log('What is the position of 534 in the array?', linearSearch(534, arr));
+console.log('What is the position of 89 in the array?', linearSearch(89, arr));
--- dsa-learning/algorithms/linear-search.ts
+++ dsa-learning/algorithms/linear-search.ts
@@ -12,5 +12,14 @@ const linearSearch = (needle: number, haystack: number[]): number => {
   return -1
 }
 
-const arr = [0, 2, 4, 12, 98, 75, 83, 3, 92, 99, 77, 33, 11, 22, 44, 534, 93, 13, 10]
-console.log(linearSearch(4, arr))
+console.log('===== Linear Search =====')
+
+const arr = [
+  0, 2, 4, 12, 98, 75, 83, 3, 92, 99, 77, 33, 11, 22, 44, 534, 93, 13, 10, 85, 84, 38, 88, 32, 10,
+]
+console.log('Array is: ')
+console.log(arr)
+console.log('What is the position of 4 in the array?', linearSearch(4, arr))
+console.log('What is the position of 99 in the array?', linearSearch(99, arr))
+console.log('What is the position of 534 in the array?', linearSearch(534, arr))
+console.log('What is the position of 89 in the array?', linearSearch(89, arr))
